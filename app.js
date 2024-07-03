8b79a3d738391aab7fb57ad650f6e105f0f818ac
j4mieLeigh <149057654+j4mieLeigh@users.noreply.github.com>
2024-07-03 17:48:40 +0200

    js practice

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
-if (dice === 1) {
-  console.log("you got one");
+function morning(name) {
+  return `Good morning ${name.toUpperCase()}`;
 }
+
+function afternoon(name) {
+  return `Good afternoon ${name.repeat(3)}`;
+}
+
+function greet(name, callback) {
+  console.log(`${callback(name)}, my name is John`);
+}
+
+greet("bobo", morning);
+greet("susy", afternoon);
+
+// function greetMorning(name) {
+//   const myName = "john";
+//   console.log(`Good morning ${name}, my name is ${myName}`);
+// }
+
+// function greetAfternoon(name) {
+//   const myName = "john";
+//   console.log(`Good afternoon ${name}, my name is ${myName}`);
+// }
+
+// greetMorning("bobo");
+// greetAfternoon("susy");
